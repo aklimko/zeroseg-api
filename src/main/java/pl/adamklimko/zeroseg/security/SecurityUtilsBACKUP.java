0f4549e7d20ac295a2230d24689e127167505cca
Adam Klimko <exusar@gmail.com>
2018-03-24 12:44:09 +0100

    Add backup configuration files

---
+++ src/main/java/pl/adamklimko/zeroseg/security/SecurityUtilsBACKUP.java
@@ -0,0 +1,8 @@
+package pl.adamklimko.zeroseg.security;
+
+public class SecurityUtilsBACKUP {
+    static final String SECRET = "SECRET";
+    static final long EXPIRATION_TIME = 259200000; // 30 days
+    static final String TOKEN_PREFIX = "Bearer ";
+    static final String HEADER_STRING = "Authorization";
+}
